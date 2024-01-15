360459a63e04901b59004c2c0ade8583a18bfe9f
lorna <lornah.gitau@student.moringaschool.com>
2024-01-15 08:56:11 +0300

    completed assignments

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ from models import db, User, Review, Game
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True  # Enable pretty-printing of JSON responses
 
 migrate = Migrate(app, db)
 
@@ -18,6 +18,49 @@ db.init_app(app)
 @app.route('/')
 def index():
     return "Index for Game/Review/User API"
+
+@app.route('/games')
+def games():
+    games = []
+    for game in Game.query.all():
+        game_dict = {
+            "title": game.title,
+            "genre": game.genre,
+            "platform": game.platform,
+            "price": game.price,
+        }
+        games.append(game_dict)
+
+    response = make_response(
+        jsonify(games),
+        200,
+        {"Content-Type": "application/json"}
+    )
+
+    return response
+
+@app.route('/games/<int:id>')
+def games_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+    if game:
+        game_dict = {
+            "title": game.title,
+            "genre": game.genre,
+            "platform": game.platform,
+            "price": game.price,
+        }
+
+        response = make_response(
+            jsonify(game_dict),
+            200
+        )
+    else:
+        response = make_response(
+            jsonify({"error": "Game not found"}),
+            404
+        )
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
